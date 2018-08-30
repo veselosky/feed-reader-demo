4bfd6e9f58340f8a609b234cade2a55f460095c8
Vince Veselosky <vince@control-escape.com>
2018-08-30 12:05:21 -0400

    You can require() a JSON file OMGDUH

--- getFeeds.js
+++ getFeeds.js
@@ -1,50 +1,42 @@
 const FeedParser = require("feedparser");
 const fetch = require("node-fetch");
 const fs = require("fs");
+const feeds = require("./public/subscriptions.json");
 
-// TODO Read list of feeds from a JSON file
-fs.readFile("public/subscriptions.json", (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  var feeds = JSON.parse(data);
-
-  feeds.forEach(feed => {
-    if (feed.items === undefined) feed.items = [];
-    fetch(feed._url)
-      .then(res => {
-        if (res.ok) {
-          const fp = new FeedParser();
-          fp.on("error", error => console.log(error));
-          fp.on("meta", meta => Object.assign(feed, meta));
-          fp.on("readable", function() {
-            // cannot use arrow func, need stream as this
-            let item;
-            while ((item = this.read())) {
-              feed.items.push(item);
+feeds.forEach(feed => {
+  if (feed.items === undefined) feed.items = [];
+  fetch(feed._url)
+    .then(res => {
+      if (res.ok) {
+        const fp = new FeedParser();
+        fp.on("error", error => console.log(error));
+        fp.on("meta", meta => Object.assign(feed, meta));
+        fp.on("readable", function() {
+          // cannot use arrow func, need stream as this
+          let item;
+          while ((item = this.read())) {
+            feed.items.push(item);
+          }
+        });
+        fp.on("end", () => {
+          fs.writeFile(
+            `public/feed/${feed._name}.json`,
+            JSON.stringify(feed),
+            err => {
+              console.log(err ? err : `${feed._name}.json saved`);
             }
-          });
-          fp.on("end", () => {
-            fs.writeFile(
-              `public/feed/${feed._name}.json`,
-              JSON.stringify(feed),
-              err => {
-                console.log(err ? err : `${feed._name}.json saved`);
-              }
-            );
-          });
-          res.body.pipe(fp);
-        } else {
-          console.log(
-            `Something went wrong fetching ${feed._name}: ${res.status} ${
-              res.statusText
-            }`
           );
-        }
-      })
-      .catch(error => {
-        console.log(`Uh oh! ${error}`);
-      });
-  });
+        });
+        res.body.pipe(fp);
+      } else {
+        console.log(
+          `Something went wrong fetching ${feed._name}: ${res.status} ${
+            res.statusText
+          }`
+        );
+      }
+    })
+    .catch(error => {
+      console.log(`Uh oh! ${error}`);
+    });
 });
