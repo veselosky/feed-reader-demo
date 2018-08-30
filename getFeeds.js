d932d8885c74e3b998c745199b700866b511ef15
Vince Veselosky <vince@control-escape.com>
2018-08-30 08:15:12 -0400

    Reduce log noise, better error reporting

--- getFeeds.js
+++ getFeeds.js
@@ -8,7 +8,6 @@ fs.readFile("public/subscriptions.json", (err, data) => {
     console.log(err);
     return;
   }
-  console.log(data);
   var feeds = JSON.parse(data);
 
   feeds.forEach(feed => {
@@ -16,28 +15,33 @@ fs.readFile("public/subscriptions.json", (err, data) => {
     fetch(feed._url)
       .then(res => {
         if (res.ok) {
-          console.log(`Got response for ${res}`);
-        }
-        const fp = new FeedParser();
-        fp.on("error", error => console.log(error));
-        fp.on("meta", meta => Object.assign(feed, meta));
-        fp.on("readable", function() {
-          // cannot use arrow func, need stream as this
-          let item;
-          while ((item = this.read())) {
-            feed.items.push(item);
-          }
-        });
-        fp.on("end", () => {
-          fs.writeFile(
-            `public/feed/${feed._name}.json`,
-            JSON.stringify(feed),
-            err => {
-              console.log(err ? err : `${feed._name}.json saved`);
+          const fp = new FeedParser();
+          fp.on("error", error => console.log(error));
+          fp.on("meta", meta => Object.assign(feed, meta));
+          fp.on("readable", function() {
+            // cannot use arrow func, need stream as this
+            let item;
+            while ((item = this.read())) {
+              feed.items.push(item);
             }
+          });
+          fp.on("end", () => {
+            fs.writeFile(
+              `public/feed/${feed._name}.json`,
+              JSON.stringify(feed),
+              err => {
+                console.log(err ? err : `${feed._name}.json saved`);
+              }
+            );
+          });
+          res.body.pipe(fp);
+        } else {
+          console.log(
+            `Something went wrong fetching ${feed._name}: ${res.status} ${
+              res.statusText
+            }`
           );
-        });
-        res.body.pipe(fp);
+        }
       })
       .catch(error => {
         console.log(`Uh oh! ${error}`);
